dacd5562eb827fb44a05ed2d41bb0fed5f8e72c8
linuxlsx <linuxlsx@gmail.com>
2022-02-05 11:48:40 +0800

    优化一个场景

---
+++ src/main/java/org/linuxlsx/algo/leetcode/leetcode/editor/cn/PalindromeLinkedListLcci.java
@@ -0,0 +1,38 @@
+package org.linuxlsx.algo.leetcode.leetcode.editor.cn;
+
+public class PalindromeLinkedListLcci {
+    public static void main(String[] args) {
+
+        Solution solution = new PalindromeLinkedListLcci().new Solution();
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public boolean isPalindrome(ListNode head) {
+
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
